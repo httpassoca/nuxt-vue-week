e7bc4bdf5d3c028a66809290d54082e5fb477089
rfreitas <trabsom.rafael@gmail.com>
2021-04-05 18:28:57 -0300

    add home components

---
+++ components/molecules/Profile.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="flex items-center ">
+		<img src="https://github.com/rafonel.png" alt="Rafael Freitas" class="mr-6 w-20 h-20 rounded-full">
+		<div class="flex flex-col w-full">
+			<strong class="text-title text-2xl"> Passoca </strong>
+			<p class="flex items-center">
+				<img src="icons/level.svg" alt="Level" class="mr-2">
+				<span class="text-base"> Level {{ level }}</span>
+			</p>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { mapState } from 'vuex';
+
+export default Vue.extend({
+	name: 'Profile',
+	computed: mapState('Challenges', ['level']),
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,33 @@
 <template>
-	<div class="container" />
+	<section class="flex flex-col lg:flex-row flex-1 lg:flex-none lg:mt-16 sm:gap-x-10 md: gap-x-20">
+		<div class="flex flex-col w-full lg:w-1/2 ">
+			<Profile />
+			<CompletedChallenges />
+		</div>
+	</section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue';
+import Profile from '~/components/molecules/Profile.vue';
 
-export default Vue.extend({});
+interface Head {
+	title: string;
+}
+
+export default Vue.extend({
+	name: 'Index',
+	head (): Head {
+		return {
+			title: 'Home | Movue It',
+		};
+	},
+	components: {
+		CompletedChallenges,
+		Profile,
+	},
+});
 </script>
 
 <style>
