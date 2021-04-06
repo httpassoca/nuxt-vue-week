8e4685a8f1f818723fc22d4ac0510f64d6e34787
rfreitas <trabsom.rafael@gmail.com>
2021-04-06 19:29:06 -0300

    add countdown component :)

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,16 @@
 		<div class="flex flex-col w-full lg:w-1/2 ">
 			<Profile />
 			<CompletedChallenges />
+			<Countdown @completed="getNewChallenge" />
+			<button v-if="hasCountdownCompleted" disabled class="button completed">
+				Cycle completed
+			</button>
+			<button v-else-if="isCountdownActive" class="button abandon" @click="setCountdownState(false)">
+				Abandon cycle
+			</button>
+			<button v-else class="button start" @click="setCountdownState(true)">
+				Start a cycle
+			</button>
 		</div>
 	</section>
 </template>
@@ -11,6 +21,11 @@
 import Vue from 'vue';
 import CompletedChallenges from '~/components/atoms/CompletedChallenges.vue';
 import Profile from '~/components/molecules/Profile.vue';
+import Countdown from '~/components/molecules/Countdown.vue';
+
+import { mapState, mapMutations } from 'vuex';
+import { Mutations as CountdownMT } from '~/store/Countdown/types';
+import { playAudio, sendNotification } from '~/utils';
 
 interface Head {
 	title: string;
@@ -26,6 +41,38 @@ export default Vue.extend({
 	components: {
 		CompletedChallenges,
 		Profile,
+		Countdown,
+	},
+	computed: {
+		...mapState('Countdown', {
+			hasCountdownCompleted: 'hasCompleted',
+			isCountdownActive: 'isActive',
+		}),
+	},
+	methods: {
+		...mapMutations({
+			setCountdownHasCompleted: `Countdown/${CountdownMT.SET_HAS_COMPLETED}`,
+			setCountdownIsActive: `Countdown/${CountdownMT.SET_IS_ACTIVE}`,
+		}),
+		setCountdownState (flag: boolean) {
+			this.setCountdownHasCompleted(false);
+			this.setCountdownIsActive(flag);
+		},
+		getNewChallenge () {
+			this.setCountdownHasCompleted(true);
+			if (Notification?.permission === 'granted') {
+				playAudio('/notification.mp3');
+				sendNotification('New Challenge!', {
+					body: 'A new challenge has started! Go complete it!',
+					icon: '/favicon.png',
+				});
+			}
+		},
+	},
+	mounted () {
+		if ('Notification' in window) {
+			Notification.requestPermission();
+		}
 	},
 });
 </script>
